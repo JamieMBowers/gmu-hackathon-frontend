d997a8a20881d633f11a5c99d592d633b6d85199
JamieMBowers <>
2026-02-21 09:49:05 -0500

    Add Vitest config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-  },
   server: {
     proxy: {
       '/api': {
